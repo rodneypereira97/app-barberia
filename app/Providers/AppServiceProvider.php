826e13bc874f35c3c49bfd8551994e405cab65a5
rodneypereira97 <rodpereira1997@gmail.com>
2025-06-01 13:30:43 -0300

    Forzar HTTPS en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,12 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+    public function boot()
+    {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 
     /**
